44ce0eb0e8cf915593293e0b01184b80f3b43b00
Rakshitha GM <158302712+Rakshgm733@users.noreply.github.com>
2025-05-22 22:38:40 +0530

    Update experties.js

--- assets/script/experties.js
+++ assets/script/experties.js
@@ -1,92 +1,32 @@
 const ExpertiseData = [
-  {
-    name: "Java",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg",
-  },
-  {
-    name: "Kotlin",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/kotlin/kotlin-original.svg",
-  },
-  {
-    name: "Android Studio",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/androidstudio/androidstudio-original.svg",
-  },
-  {
-    name: "PHP",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg",
-  },
-  {
-    name: "Node Js",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg",
-  },
-  {
-    name: "React Js",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg",
-  },
-  {
-    name: "Firebase",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-original.svg",
-  },
-  {
-    name: "Cloudflare",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cloudflare/cloudflare-original.svg",
-  },
-  {
-    name: "Javascript",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
-  },
+  
   {
     name: "MySQL",
     logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original.svg",
   },
+
   {
-    name: "Next Js",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg",
-  },
-  {
-    name :"Netlify",
-    logo :"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/netlify/netlify-original.svg"
-  },
-  {
-    name: "Postman",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg",
-  },
-  {
-    name: "Sass",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg",
-  },
-  {
-    name: "Wordpress",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/wordpress/wordpress-plain.svg",
-  },
-  {
-    name: "Stackoverflow",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/stackoverflow/stackoverflow-original.svg",
-  },
-  {
-    name :"JSON",
-    logo :"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/json/json-plain.svg"
-  },
-  {
-    name: "Intellij",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/intellij/intellij-original.svg",
-  },
-  {
-    name: "Canva",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/canva/canva-original.svg",
-  },
-  {
-    name: "Eclipse",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/eclipse/eclipse-original.svg",
-  },
-  {
-    name: "Git",
-    logo: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
-  },
-  {
-    name :"Tailwindcss",
-    logo :"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg"
-  }
+  name: "C++",
+  logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg",
+},
+{
+  name: "Python",
+  logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+},
+{
+  name: "HTML",
+  logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+},
+{
+  name: "CSS",
+  logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+},
+{
+  name: "OOPs",
+  logo: "https://cdn-icons-png.flaticon.com/512/919/919828.png",  // Simple OOP/Classes icon from flaticon
+},
+
+ 
 ];
 
 const ExpertiseDataHTML = ExpertiseData.map(
